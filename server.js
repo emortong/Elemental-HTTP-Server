9f5931c65d3aba9a6e93f097338b628f750aa064
Estefania Morton <estefania.morton@gmail.com>
2016-12-31 10:06:53 -1000

    PUT method works

--- server.js
+++ server.js
@@ -2,58 +2,102 @@ const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring');
 
-let existingFiles = fs.readdirSync('./public');
+let existingFiles = null;
 
 const server = http.createServer((req,res) => {
-  res.setHeader('Content-Type', 'text/html');
-  console.log(req.method)
-  if(req.url !== '/') {
-    req.url = req.url.substring(1);
-  }
 
-  if(req.method === 'POST' && req.url === 'element') {
-    let rawData = '';
+  function setResponse() {
+    res.setHeader('Content-Type', 'text/html');
+    console.log(req.method)
+    if(req.url !== '/') {
+      req.url = req.url.substring(1);
+    }
+    let exists = false;
 
-    req.on('data', (data) => {
-      let exists = false;
-      rawData += data;
-      let parsedData = querystring.parse(rawData);
-      console.log(parsedData);
-      let fileName = `${parsedData.elementName.toLowerCase()}.html`;
-      console.log(fileName);
-      let elementName = parsedData.elementName;
-      let elementSymbol = parsedData.elementSymbol;
-      let elementAtomicNumber = parsedData.elementAtomicNumber;
-      let elementDescription = parsedData.elementDescription;
-      let template = generateTemplate(elementName, elementSymbol, elementAtomicNumber, elementDescription)
+    if(req.method === 'POST' && req.url === 'element') {
+      let rawData = '';
+      req.on('data', (data) => {
+        // let exists = false;
+        rawData += data;
+        let parsedData = querystring.parse(rawData);
+        console.log(parsedData);
+        let fileName = `${parsedData.elementName.toLowerCase()}.html`;
+        console.log(fileName);
+        let elementName = parsedData.elementName;
+        let elementSymbol = parsedData.elementSymbol;
+        let elementAtomicNumber = parsedData.elementAtomicNumber;
+        let elementDescription = parsedData.elementDescription;
+        let template = generateTemplate(elementName, elementSymbol, elementAtomicNumber, elementDescription)
 
-      existingFiles.forEach((x) => {
-        if(x === fileName) {
-          exists = true;
+        existingFiles.forEach((x) => {
+          if(x === fileName) {
+            exists = true;
+          }
+        })
+
+        if(!exists) {
+          let fileWriteStream = fs.createWriteStream(`./public/${fileName}`)
+          fs.writeFile(`./public/${fileName}`, template, (err) => {
+            if (err) throw err;
+          })
+          res.writeHead(200, { 'Content-Type': 'application/json'});
+          let li = `<li>\n<a href="${fileName}">${elementName}</a>\n</li>`
+          createNewLi(li);
+          res.end(`{ "success" : true }`);
+        } else {
+          res.end(`{ "success" : false }`);
         }
       })
+    } // close POST method conditional
 
-      if(!exists) {
-        let fileWriteStream = fs.createWriteStream(`./public/${fileName}`)
-        fs.writeFile(`./public/${fileName}`, template, (err) => {
-          if (err) throw err;
+    if(req.method === 'GET') {
+        console.log(req.url);
+        // let exists = false;
+        if(req.url === '/') {
+          req.url = 'index.html'
+        }
+
+        existingFiles.forEach((x) => {
+          if(x === req.url) {
+            exists = true;
+          }
         })
-        res.writeHead(200, { 'Content-Type': 'application/json'});
-        let li = `<li>\n<a href="${fileName}">${elementName}</a>\n</li>`
-        createNewLi(li);
-        res.end(`{ "success" : true }`);
-      } else {
-        res.end(`{ "success" : false }`);
-      }
-    })
-  } // close POST method conditional
 
-  if(req.method === 'GET') {
-      console.log(req.url);
-      let exists = false;
-      if(req.url === '/') {
-        req.url = 'index.html'
-      }
+        if(req.url.endsWith('.css')) {
+          res.setHeader('Content-Type', 'text/css')
+          exists = true;
+        }
+
+        if(exists) {
+          fs.readFile(`./public/${req.url}`, (err, fileContent) => {
+            if (err) throw err;
+            res.write(fileContent);
+            res.end();
+          });
+        } else {
+          fs.readFile(`./public/404.html`, (err, fileContent) => {
+            if (err) throw err;
+            res.statusCode = 404;
+            res.write(fileContent);
+            res.end();
+          })
+        }
+    }
+
+    if(req.method === 'PUT') {
+    let rawData = '';
+
+     req.on('data', (data) => {
+        rawData += data;
+        let parsedData = querystring.parse(rawData);
+        console.log(parsedData);
+        let fileName = `${parsedData.elementName.toLowerCase()}.html`;
+        console.log(fileName);
+        let elementName = parsedData.elementName;
+        let elementSymbol = parsedData.elementSymbol;
+        let elementAtomicNumber = parsedData.elementAtomicNumber;
+        let elementDescription = parsedData.elementDescription;
+        let template = generateTemplate(elementName, elementSymbol, elementAtomicNumber, elementDescription)
 
       existingFiles.forEach((x) => {
         if(x === req.url) {
@@ -61,26 +105,35 @@ const server = http.createServer((req,res) => {
         }
       })
 
-      if(req.url.endsWith('.css')) {
-        res.setHeader('Content-Type', 'text/css')
-        exists = true;
-      }
+       if(exists) {
+          let fileWriteStream = fs.readFile(`./public/${req.url}`)
+          fs.writeFile(`./public/${req.url}`, template, (err) => {
+            if (err) throw err;
+          })
+          fs.rename(`./public/${req.url}`,`./public/${fileName}`, (err) => {
+            if (err) throw err;
+          })
+          res.writeHead(200, { 'Content-Type': 'application/json'});
+          let li = `<li>\n<a href="${fileName}">${elementName}</a>\n</li>`
+          // createNewLi(li);
+          res.end(`{ "success" : true }`);
+        } else {
+          res.end(`{ "success" : false }`);
+        }
+      });
 
-      if(exists) {
-        fs.readFile(`./public/${req.url}`, (err, fileContent) => {
-          if (err) throw err;
-          res.write(fileContent);
-          res.end();
-        });
-      } else {
-        fs.readFile(`./public/404.html`, (err, fileContent) => {
-          if (err) throw err;
-          res.statusCode = 404;
-          res.write(fileContent);
-          res.end();
-        })
-      }
-  }
+
+    }
+
+
+  } // setResponse
+
+  fs.readdir('./public', (err, files) => {
+      if (err) throw err;
+      existingFiles = files;
+      setResponse(); // everything is called from here bc async
+    });
+
 }); // closing bracket for createServer
 
 server.listen(8080, () => {
